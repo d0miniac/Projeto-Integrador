4a72956f4003ed920ff9bbb48d663a3b07998259
henrique <henrique.l19@aluno.ifsc.edu.br>
2025-04-07 12:04:42 -0300

    Layout Clientes

--- src/visao/TelaCliente.java
+++ src/visao/TelaCliente.java
@@ -7,11 +7,14 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class TelaCliente extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -43,9 +46,33 @@ public class TelaCliente extends JFrame {
 		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, "cell 0 0,grow");
+		panel.setLayout(new MigLayout("", "[][]", "[][]"));
+		
+		JButton btnNewButton = new JButton("New button");
+		panel.add(btnNewButton, "cell 0 1");
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		panel.add(lblNewLabel_1, "cell 1 1");
 		
 		JPanel panel_1 = new JPanel();
 		contentPane.add(panel_1, "cell 0 1,grow");
+		panel_1.setLayout(new MigLayout("", "[grow][][][][][][][][]", "[][]"));
+		
+		textField = new JTextField();
+		panel_1.add(textField, "cell 0 0,growx");
+		textField.setColumns(10);
+		
+		JButton btnpesquisa = new JButton("PESQUISAR");
+		panel_1.add(btnpesquisa, "cell 8 0");
+		
+		JButton btnCadastro = new JButton("Cadastrar");
+		panel_1.add(btnCadastro, "cell 0 1");
+		
+		JButton btnAltera = new JButton("Alterar");
+		panel_1.add(btnAltera, "cell 1 1");
+		
+		JButton btnDeletar = new JButton("Deletar");
+		panel_1.add(btnDeletar, "cell 2 1");
 	}
 
 }
